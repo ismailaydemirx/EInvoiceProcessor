6367c8c0d4e712c527c24b764df235dbe43d1978
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-16 15:27:35 +0300

    Filter Invoice and update values

--- EInvoiceProcessor/Program.cs
+++ EInvoiceProcessor/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using EInvoiceProcessor.Envelope;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 using UblSharp;
 
@@ -8,20 +9,46 @@ class Program()
 {
     static void Main()
     {
-        var invoices = new List<string>
+        #region Create an envelope with the given invoice files
+        //var invoices = new List<string>
+        //{
+        //    Path.Combine("Invoices", "Invoice1.xml"),
+        //    Path.Combine("Invoices", "Invoice2.xml"),
+        //};
+
+        //EnvelopeBuilder.CreateEnvelope
+        //    (
+        //    invoiceFilePaths: invoices,
+        //    sender: "1234567890",
+        //    reciever: "0987654321",
+        //    outputPath: "../../../envelope.xml");
+
+        //Console.WriteLine("Envelope created: envelope.xml");
+        #endregion
+        #region Filter Invoice and update values
+        var xmlPath = "../../../Invoices/invoices.xml";
+
+        XElement root = XElement.Load(xmlPath);
+
+        var filteredInvoice = root.Elements("Invoice")
+            .FirstOrDefault(inv => inv.Element("ID")?.Value == "INV-001");
+
+        if (filteredInvoice != null)
         {
-            Path.Combine("Invoices", "Invoice1.xml"),
-            Path.Combine("Invoices", "Invoice2.xml"),
-        };
+            filteredInvoice.Element("Total")!.Value = "2000.00";
+            filteredInvoice.Element("Supplier")!.Value = "New Supplier Name";
 
-        EnvelopeBuilder.CreateEnvelope
-            (
-            invoiceFilePaths: invoices,
-            sender: "1234567890",
-            reciever: "0987654321",
-            outputPath: "../../../envelope.xml");
+            root.Save(xmlPath);
 
-        Console.WriteLine("Envelope created: envelop.xml");
+            Console.WriteLine("Invoice is updated");
+
+        }
+        else
+        {
+            Console.WriteLine("Invoice not found.");
+        }
+        #endregion
+
 
     }
 }
--- EInvoiceProcessor/obj/Debug/net9.0/EInvoiceProcessor.AssemblyInfo.cs
+++ EInvoiceProcessor/obj/Debug/net9.0/EInvoiceProcessor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7085e3e5e7c9ff58a3942fa4187a73bdff482f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
